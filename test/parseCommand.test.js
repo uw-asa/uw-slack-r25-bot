ac9c6a83e51bd39f6013a8503c40e7c24ebb360b
Chase Sawyer <shadowimmage@gmail.com>
2018-04-25 12:46:19 -0700

    Created separate parser to handle expanded commands for version 2. added utility file for long static text responses. Created test suite for command parsing functions

---
+++ test/parseCommand.test.js
@@ -0,0 +1,109 @@
+
+const expect = require('chai').expect
+
+const parser = require('../utils/parseCommand').parseCommand
+
+describe('parseCommand(queryText)', function () {
+  it('Number of command elements counted correctly.', function () {
+    const testStrings = [
+      'this is a 6 element string',
+      'this  string  has  double  spaces',
+      'this string   has a   mixture of  spaces',
+      'only two',
+      'one',
+    ]
+    const expectedCounts = [6, 5, 7, 2, 1]
+    for (var i = 0; i < testStrings.length; i++) {
+      var result = parser(testStrings[i])
+      expect(result.numberOfElements).to.equal(expectedCounts[i])
+    }
+  })
+
+  it('Expect querySpace to always contain the first two elements if present', function () {
+    const testStrings = [
+      'this is a 6 element string',
+      'one Two',
+      'OnE Two ThrEE',
+      'only two',
+      'one 5161516',
+    ]
+    const expectedQuerySpaceStrings = [
+      'THIS IS',
+      'ONE TWO',
+      'ONE TWO',
+      'ONLY TWO',
+      'ONE 5161516'
+    ]
+    for (var i = 0; i < testStrings.length; i++) {
+      var result = parser(testStrings[i])
+      expect(result.querySpace).to.equal(expectedQuerySpaceStrings[i])
+    }
+  })
+
+  it('Expect a roomId if command length >= 2 and first 2 params are valid', function () {
+    var result = parser('arc 147')
+    expect(result.numberOfElements).to.equal(2)
+    expect(result.roomId).to.not.be.null
+    expect(result.roomId).to.equal('6063')
+    result = parser('arc 222')
+    expect(result.roomId).to.be.null
+  })
+
+  it('Expect help text if given the \'help\' command', function () {
+    var result = parser('help')
+    expect(result.resolvedCommandText).to.be.a('string')
+    expect(result.resolvedCommand).to.equal('HELP')
+  })
+
+  it('Expect \'ERROR\' non-help, short command', function () {
+    var result = parser('nothelp')
+    expect(result.resolvedCommand).to.equal('ERROR')
+    expect(result.resolvedCommandText).to.not.be.null
+  })
+
+  it('Expect \'ERROR\' resolved command for 2 incorrect params', function () {
+    var result = parser('one two')
+    expect(result.resolvedCommand).to.equal('ERROR')
+    expect(result.resolvedCommandText).to.not.be.null
+  })
+
+  it('Expect \'SCHEDULE\', roomID, and dayDeltaStr pf \'+1\' for valid \'...tomorrow\' query', function () {
+    var result = parser('KNE 130 tomorrow')
+    expect(result.numberOfElements).to.equal(3)
+    expect(result.resolvedCommand).to.equal('SCHEDULE')
+    expect(result.roomId).to.equal('4992')
+    expect(result.args.dayDeltaStr).to.equal('+1')
+  })
+
+  it('Expect \'BREAKS\' resolved command for valid query room for \'breaks\'', function () {
+    var result = parser('KNE 210 breaks')
+    expect(result.numberOfElements).to.equal(3)
+    expect(result.roomId).to.equal('4993')
+    expect(result.resolvedCommand).to.equal('BREAKS')
+    expect(result.args.allBreaks).to.equal(true)
+  })
+
+  it('Expect \'SCHEDULE\' and correct parsing of \'+[dayoffset]\' with numerical offset', function () {
+    var result = parser('ARC 147 +2')
+    expect(result.numberOfElements).to.equal(3)
+    expect(result.resolvedCommand).to.equal('SCHEDULE')
+    expect(result.roomId).to.equal('6063')
+    expect(result.args.dayDeltaStr).to.equal('+2')
+  })
+
+  it('Expect no day delta and ERROR for \'+{non-numeral}\' on otherwise correct query', function () {
+    var result = parser('ARC 147 +three')
+    expect(result.numberOfElements).to.equal(3)
+    expect(result.args.dayDeltaStr).to.be.null
+    expect(result.resolvedCommand).to.equal('ERROR')
+    expect(result.resolvedCommandText).to.not.be.null
+  })
+
+  it('Expect \'...next break\' to parse correctly', function () {
+    var result = parser('KNE 130 next break')
+    expect(result.numberOfElements).to.equal(4)
+    expect(result.resolvedCommand).to.equal('BREAKS')
+    expect(result.args.allBreaks).to.equal(false)
+  })
+
+})
---
+++ utils/parseCommand.js
@@ -0,0 +1,77 @@
+/* helpful module for parsing out the received command, and helping the main handler
+  function decide what to do and what to return to slack/client.
+  Possible command types are some flavor of:
+    - help
+    - get schedule
+    - get breaks
+*/
+const getRoomId = require('./getRoomId')
+const responseText = require('./responseText.json') // long blobs of static text to be returned to the user
+
+function parseCommand(queryText) {
+  queryText = queryText.trim().toUpperCase()
+  var command = {
+    elements: null,
+    numberOfElements: null,
+    resolvedCommand: null,
+    resolvedCommandText: null,
+    querySpace: null,
+    roomId: null,
+    args: {
+      dayDeltaStr: null,
+      allBreaks: true
+    }
+  }
+
+  command.elements = queryText.split(/\s+/)
+  command.numberOfElements = command.elements.length
+
+  if (command.numberOfElements < 2 && command.elements[0] == 'HELP') {
+    command.resolvedCommand = 'HELP'
+    command.resolvedCommandText = responseText.text['HELP']
+
+  } else if (command.numberOfElements >= 2) {
+    command.querySpace = command.elements[0] + ' ' + command.elements[1]
+    command.roomId = getRoomId(command.querySpace)
+    command.resolvedCommand = 'SCHEDULE'
+
+    if (command.roomId == null) {
+      command.resolvedCommand = 'ERROR'
+      command.resolvedCommandText = responseText.text['ERROR-ROOM-QUERY']
+
+    } else if (command.numberOfElements > 2 && command.elements[2].length >= 2) {
+      /* possible extended commands: 
+        ...['breaks'] == show all breaks
+        ...['next break'] == show the next break only
+        ...['tomorrow'] == special - give tomorrow's times
+        ...['+1' | '+2' | etc.] == day delta
+        ...['02/13/2018'] -- parse and find actual date
+      */
+      if (command.elements[2] == 'TOMORROW') {
+        command.args.dayDeltaStr = '+1'
+      } else if (command.elements[2][0] == '+' && !isNaN(parseInt(command.elements[2].slice(1)))) {
+        command.args.dayDeltaStr = command.elements[2]
+
+      } else if (command.elements[2] == 'BREAKS' || command.elements[2] == 'NEXT') {
+        command.resolvedCommand = 'BREAKS'
+        if (command.elements[2] == 'NEXT' && command.numberOfElements >= 4 && command.elements[3] == 'BREAK') {
+          command.args.allBreaks = false
+        }
+      } else {
+        // wasn't able to parse extended command
+        command.resolvedCommand = 'ERROR'
+        command.resolvedCommandText = responseText.text['ERROR-EXTENDED-COMMAND']
+      }
+    }
+  } else {
+    // short command, wasn't for help
+    command.resolvedCommand = 'ERROR'
+    command.resolvedCommandText = responseText.text['ERROR-GENERIC']
+  }
+
+  return command
+}
+
+module.exports = {
+  parseCommand
+}
