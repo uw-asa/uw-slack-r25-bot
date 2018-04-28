b3b8220061a0f6cd8af7441d1a0f923f082ac8cd
Chase Sawyer <shadowimmage@gmail.com>
2018-04-27 17:32:10 -0700

    Added more helpers

---
+++ test/datetimeUtils.test.js
@@ -0,0 +1,42 @@
+
+const expect = require('chai').expect
+const datetimeUtils = require('../utils/datetimeUtils')
+
+describe('datetimeUtils.js getTimeFromDateTime()', function () {
+  it('Expect time portion of datetime string', function () {
+    var result = datetimeUtils.getTimeFromDateTime('2018-04-18T09:30:00-07:00')
+    expect(result).to.equal('09:30:00')
+  })
+})
+
+describe('datetimeUtils.js getDateStrFromDayDelta()', function () {
+  it('Expect locale date string to be offset by 1 for \'+1\'', function () {
+    var result = datetimeUtils.getDateStrFromDayDelta('+1')
+    var todayEpoch = new Date().getTime()
+    var expectedResult = new Date(todayEpoch + 86400000).toLocaleDateString()
+    expect(result).to.equal(expectedResult)
+  })
+
+  it('Expect no offset if offset parameter is malformed', function () {
+    var result = datetimeUtils.getDateStrFromDayDelta('5')
+    var expectedResult = new Date().toLocaleDateString()
+    expect(result).to.equal(expectedResult)
+  })
+})
+
+describe('datetimeUtils.js timeStrDiffMin()', function () {
+  it('Expect an integer response for two correct time string params.', function () {
+    var result = datetimeUtils.timeStrDiffMin('10:00:00', '10:30:00')
+    expect(result).to.equal(30)
+  })
+
+  it('Expect a negative time difference for end time before start time.', function () {
+    var result = datetimeUtils.timeStrDiffMin('10:30:00', '10:00:00')
+    expect(result).to.equal(-30)
+  })
+
+  it('Expect correct parsing of 24-hr times', function () {
+    var result = datetimeUtils.timeStrDiffMin('13:00:00', '14:00:00')
+    expect(result).to.equal(60)
+  })
+})
--- utils/datetimeUtils.js
+++ utils/datetimeUtils.js
@@ -1,4 +1,6 @@
 
+const MS_IN_DAY = 86400000
+const MS_IN_MINUTE = 60000
 
 function getTimeFromDateTime(dateTimeStr) {
   /* Returns the time component of a datetime string without any timezone info.
@@ -9,6 +11,46 @@ function getTimeFromDateTime(dateTimeStr) {
   return date.toLocaleTimeString()
 }
 
+/*
+function getDayDeltaStrFromDate(dateStr) {
+  /* Parse the difference between a specified date and today, returning the
+  time difference in days.*
+  var date = new Date(dateStr)
+  if (date != 'Invalid Date') {
+    
+  }
+  // TODO: how to handle error here?
+}
+*/
+function getDateStrFromDayDelta(deltaStr) {
+  /* Take a day delta str, and add that number of days to the current date.
+    deltaStr must be in the form of '+#' where # is an integer. */
+  if (deltaStr[0] == '+' && deltaStr.length > 1 && !isNaN(parseInt(deltaStr.slice(1)))) {
+    var timeDiffMs = parseInt(deltaStr.slice(1)) * MS_IN_DAY
+    var todayEpoch = new Date().getTime()
+    return new Date(todayEpoch + timeDiffMs).toLocaleDateString()
+  } else {
+    return new Date().toLocaleDateString()
+  }
+}
+
+function timeStrDiffMin(beginTime, endTime) {
+  /* take two time strings and give the difference in minutes.
+    Expects time strings to be in the form: 'HH:MM:SS' */
+  beginTime = beginTime.split(':')
+  endTime = endTime.split(':')
+  // create dates with only the times filled in
+  var beginEpoch = new Date(0, 0, 0, beginTime[0], beginTime[1], beginTime[2], 0).getTime()
+  var endEpoch = new Date(0, 0, 0, endTime[0], endTime[1], endTime[2], 0).getTime()
+  var timeDiff = endEpoch - beginEpoch
+  timeDiff = timeDiff / MS_IN_MINUTE
+  // console.log(timeDiff + ' mins')
+  return timeDiff
+}
+
 module.exports = { 
-  getTimeFromDateTime
+  getTimeFromDateTime,
+  // getDayDeltaStrFromDate,
+  getDateStrFromDayDelta,
+  timeStrDiffMin
 }
