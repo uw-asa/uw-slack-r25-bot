926190a81df5b3b004b52263161439feff9e4a14
Chase Sawyer <shadowimmage@gmail.com>
2018-04-27 17:31:52 -0700

    updated to use new command and response handler flows

--- handler.js
+++ handler.js
@@ -2,17 +2,19 @@
 
 'use strict'
 
-
-const getRoomId = require('./utils/getRoomId')
-const r25ws = require('./utils/r25ws')
 const querystring = require('querystring')
-const processSchedule = require('./utils/processSchedule')
-const postSchedule = require('./utils/postSchedule')
+
+const r25ws = require('./utils/r25ws')
+const processSchedule = require('./utils/r25wsResponseHandler').processSchedule
+const processBreaks = require('./utils/r25wsResponseHandler').processBreaks
+const parser = require('./utils/commandParser')
+const postData = require('./utils/postData')
 
 module.exports.getTimes = (event, context, callback) => {
   // console.log(event); // Contains incoming request data (e.g., query params, headers and more)
   const payload = querystring.parse(event.body)
   if (payload.token != process.env.SLACK_TOKEN) {
+    // Fail early if token is incorrect / missing / etc.
     const response = {
       statusCode: 200,
       headers: {
@@ -20,17 +22,15 @@ module.exports.getTimes = (event, context, callback) => {
       },
       body: JSON.stringify({
         'response_type': 'ephemeral',
-        'text': 'Error: unauthorized, token invalid, check app configuration\n'
+        'text': 'Error: Unauthorized; token invalid, check app configuration\n'
       })
     }
+    console.log('Bad request received with token: ' + payload.token)
     callback(null, response)
   } else {
-    // check if the room requested is in the list
-    //    if so, send good response, else send error and end.
-    const queryText = payload.text.trim()
-    const roomId = getRoomId
-  (queryText)
-    if (roomId == null) {
+    // Parse command and return with result to caller 
+    const command = parser.parseCommand(payload.text)
+    if (command.resolvedCommand == 'ERROR' || command.resolvedCommand == 'HELP') {
       const response = {
         statusCode: 200,
         headers: {
@@ -38,25 +38,38 @@ module.exports.getTimes = (event, context, callback) => {
         },
         body: JSON.stringify({
           'response_type': 'ephemeral',
-          'text': 'Error: incorrect search parameter - did you search for something like "KNE 130"?'
+          'text': command.resolvedCommandText
         })
       }
       callback(null, response)
+      if (command.resolvedCommand == 'ERROR') {
+        console.log('Error: ' + command.resolvedCommandText)
+      }
     } else {
-      // send OK response, then process times...
-      let response = {
-        statusCode: 200
-        // headers: {
-        //   'content-type': 'application/json'
-        // },
-        // body: JSON.stringify({
-        //   'response_type': 'ephemeral',
-        //   'text': 'Getting times from R25...'
+      // send OK response, then process parsed command.
+      const response = {
+        statusCode: 200,
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          // this should cause the command to be echoed back in channel too.
+          'response_type': 'in_channel',
+          'text': 'Getting times from R25...'
+        })
       }
       callback(null, response)
-      r25ws.getTimesForId(roomId, function(results) {
-        const scheduleData = processSchedule(results, queryText)
-        postSchedule(scheduleData, payload.response_url)
+      r25ws.getTimesForId(command, function (results) {
+        var data = null
+        if (command.resolvedCommand == 'SCHEDULE') {
+          data = processSchedule(results, command)
+        } else if (command.resolvedCommand == 'BREAKS') {
+          data = processBreaks(results, command)
+        } else {
+          // error
+          console.log('Error: resolvedCommand after getTimesForId failed to parse')
+        }
+        postData(data, payload.response_url)
       })
     }
   }
